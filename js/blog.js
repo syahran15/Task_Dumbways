ff5b2c29fe0c0423b31b3057906ad0ad8cf9a3ea
Ahmad Syahran Zidane <zidanenoor17@gmail.com>
2023-05-23 22:44:12 +0700

    push task_day5_week2

--- js/blog.js
+++ js/blog.js
@@ -11,10 +11,26 @@ function addBlog(event) {
 
   // fitur durasi
 
-  let start = new Date(startDate);
-  let end = new Date(endDate);
-  let diff = end.getTime() - start.getTime();
-  let duration = Math.ceil(diff / (1000 * 3600 * 24 * 30));
+
+  let distance = endDate - startDate; // hasilnya milidetik
+  console.log(distance);
+
+  let milisecond = 1000; // milisecond
+  let secondInHours = 3600; // 1 jam 3600 detik
+  let hoursInDays = 24; // 1 hari 24 jam
+  let daysInMonth = 30; // 30 hari sebulan
+  let monthInYear = 12; // 12 bulan setahun
+
+  let distanceDay = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays)
+  ); 
+  let distanceMonth = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays * daysInMonth)
+  );
+  let distanceYear = Math.floor(
+    distance /
+      (milisecond * secondInHours * hoursInDays * daysInMonth * monthInYear)
+  );
 
   //untuk menampilkan gambar
   image = URL.createObjectURL(image[0]);
@@ -22,15 +38,19 @@ function addBlog(event) {
 
   let blog = {
     projectName,
-    duration,
+    distanceDay,
+    distanceMonth,
+    distanceYear,
     description,
+    distance,
+    realTime : new Date(),
     image,
   };
 
   dataBlog.push(blog);
   console.log(dataBlog);
 
-  renderBlog()
+  renderBlog();
 }
 
 function renderBlog() {
@@ -46,7 +66,7 @@ function renderBlog() {
                 <img src="${dataBlog[index].image}" alt="blog_img" />
               </div>
               <h3 class="card-title">${dataBlog[index].projectName}</h3>
-              <p class="card-duration">Durasi : ${dataBlog[index].duration} hari </p>
+              <p class="card-duration">Durasi : ${dataBlog[index].distanceDay} Day , ${dataBlog[index].distanceMonth} Month , ${dataBlog[index].distanceYear} Year </p>
               <br />
               <p class="card-description">
                 ${dataBlog[index].description}
@@ -60,8 +80,76 @@ function renderBlog() {
                 <button class="btn-edit">Edit</button>
                 <button class="btn-delete">Delete</button>
               </div>
+              <div style="float: right; margin: 10px">
+              <p style="font-size: 15px; color: grey">${getRealTime(dataBlog[index].realTime)}</p>
+            </div>
             </div>
           </div>
     `;
   }
 }
+
+function getFullTime(time) {
+  let monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Desc",
+  ];
+
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours <= 9) {
+    hours = "0" + hours;
+  } else if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
+  return `${date} ${monthName[monthIndex]} ${year} ${hours} : ${minutes} WIB`;
+}
+
+function getRealTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+
+  // waktu sekarang - waktu post
+  let realTime = timeNow - timePost; // hasilnya milidetik
+  console.log(realTime);
+
+  let milisecond = 1000; // milisecond
+  let secondInHours = 3600; // 1 jam 3600 detik
+  let hoursInDays = 24; // 1 hari 24 jam
+
+  let realTimeDay = Math.floor(
+    realTime / (milisecond * secondInHours * hoursInDays)
+  ); // 1/86400000
+  let realTimeHours = Math.floor(realTime / (milisecond * 60 * 60)); // 1/3600000
+  let realTimeMinutes = Math.floor(realTime / (milisecond * 60)); // 1/60000
+  let realTimeSecond = Math.floor(realTime / milisecond); // 1/1000
+
+  if (realTimeDay > 0) {
+    return `${realTimeDay} Day Ago`;
+  } else if (realTimeHours > 0) {
+    return `${realTimeHours} Hours Ago`;
+  } else if (realTimeMinutes > 0) {
+    return `${realTimeMinutes} Minutes Ago`;
+  } else {
+    return `${realTimeSecond} Seconds Ago`;
+  }
+}
+
+setInterval(function () {
+  renderBlog();
+}, 2000);
